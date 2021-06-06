2c30e4af8a09feae026fa84b89aab00b90bf223c
NerdyCoder-AJ <akash.codeway@gmail.com>
2021-06-06 18:09:07 +0530

    master

--- tubers/webpages/views.py
+++ tubers/webpages/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render, HttpResponse, HttpResponseRedirect
 from .models import Slider, Team
+from youtubers.models import Youtuber
+
 # Create your views here.
 def home(request):
     sliders = Slider.objects.all()
     teams = Team.objects.all()
-    data = {'sliders': sliders, 'teams': teams}
+    featured_youtuber = Youtuber.objects.order_by('-created_date').filter(is_featured=True)
+    all_tubers = Youtuber.objects.order_by('-created_date')
+    data = {
+        'sliders': sliders,
+         'teams': teams,
+         'featured_youtuber': featured_youtuber,
+         'all_tubers': all_tubers,
+         }
     return render(request, 'webpages/home.html', data)
 
 def about(request):
--- tubers/youtubers/admin.py
+++ tubers/youtubers/admin.py
@@ -3,6 +3,7 @@ from . import models
 # Register your models here.
 @admin.register(models.Youtuber)
 class YoutuberAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'price', 'category', 'is_featured')
+    list_display = ('id', 'name', 'price', 'is_featured')
     search_fields = ('id', 'name', 'category')
-    list_filter = ('id', 'name', 'price', 'category')
+    list_filter = ('id', 'name', 'price')
+    list_editable = ('is_featured',)
