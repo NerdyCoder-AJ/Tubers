e780fce51345e21b6c59571db09e3f762f2cb558
NerdyCoder-AJ <akash.codeway@gmail.com>
2021-06-08 10:24:26 +0530

    master

--- tubers/youtubers/views.py
+++ tubers/youtubers/views.py
@@ -1,11 +1,19 @@
-from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404, render
+from . models import Youtuber
 # Create your views here.
 def youtubers(request):
-    pass
+    tubers = Youtuber.objects.order_by('-created_date')
+    data = {
+        'tubers': tubers,
+    }
+    return render(request, 'youtubers/youtubers.html', data)
 
-def youtubers_details(request):
-    pass
+def youtubers_details(request, id):
+    tuber = get_object_or_404(Youtuber, pk=id)
+    data = {
+        'tuber':tuber,
+    }  
+    return render(request, 'youtubers/youtuber_detail.html', data)
 
 def youtubers_search(request):
     pass
